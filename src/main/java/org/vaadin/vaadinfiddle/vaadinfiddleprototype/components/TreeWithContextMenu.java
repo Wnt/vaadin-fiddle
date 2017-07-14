ce603214fb27c43c4cfd7e5bb83516b07d8748b7
Jonni Nakari <jonni@vaadin.com>
2017-07-14 16:58:53 +0200

    Tree setHeight* methods to workaround vaadin/framework#9629

--- src/main/java/org/vaadin/vaadinfiddle/vaadinfiddleprototype/components/TreeWithContextMenu.java
+++ src/main/java/org/vaadin/vaadinfiddle/vaadinfiddleprototype/components/TreeWithContextMenu.java
@@ -5,13 +5,29 @@ import com.vaadin.ui.Tree;
 import com.vaadin.ui.TreeGrid;
 
 public class TreeWithContextMenu<T> extends Tree<T> {
-		private GridContextMenu<T> gridContextMenu;
+	private GridContextMenu<T> gridContextMenu;
 
-		public GridContextMenu<T> getContextMenu() {
-			if (gridContextMenu == null) {
-				TreeGrid<T> treegrid = (TreeGrid) getCompositionRoot();
-				gridContextMenu = new GridContextMenu<>(treegrid);
-			}
-			return gridContextMenu;
+	public GridContextMenu<T> getContextMenu() {
+		if (gridContextMenu == null) {
+			TreeGrid<T> treegrid = (TreeGrid) getCompositionRoot();
+			gridContextMenu = new GridContextMenu<>(treegrid);
 		}
+		return gridContextMenu;
 	}
+
+	// setHeight* methods to workaround vaadin/framework#9629
+	@Override
+	public void setHeight(float height, Unit unit) {
+		getCompositionRoot().setHeight(height, unit);
+	}
+
+	@Override
+	public void setHeight(String height) {
+		getCompositionRoot().setHeight(height);
+	}
+
+	@Override
+	public void setHeightUndefined() {
+		getCompositionRoot().setHeightUndefined();
+	}
+}
