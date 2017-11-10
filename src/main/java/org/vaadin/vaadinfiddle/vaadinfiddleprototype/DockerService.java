2200c015c5c9c7f5d0647fdd090e17a7e404dcf7
Jonni Nakari <jonni@vaadin.com>
2017-11-10 17:39:14 +0100

    Don't clean compiled widget set on save, just remove all .class files.

--- src/main/java/org/vaadin/vaadinfiddle/vaadinfiddleprototype/DockerService.java
+++ src/main/java/org/vaadin/vaadinfiddle/vaadinfiddleprototype/DockerService.java
@@ -257,7 +257,7 @@ public class DockerService {
 		DockerClient throwAwayDockerClient = DockerClientBuilder.getInstance(configBuilder.build()).build();
 
 		ExecCreateCmd cmdBuild = throwAwayDockerClient.execCreateCmd(id).withCmd("su", "vaadin", "-c",
-				"cd /webapp/fiddleapp; mvn clean jetty:run");
+				"cd /webapp/fiddleapp; find target -name \"*.class\" | xargs rm ; mvn jetty:run");
 		if (stdout != null) {
 			cmd = cmdBuild.withAttachStdout(true).exec();
 
